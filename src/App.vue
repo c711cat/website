1703d7f02c0de827c85fa33c48ddf4258af6f478
c711cat <c711cat@gmail.com>
2023-11-15 16:18:41 +0800

    將Navbar.vue的元件載入App.vue中呈現

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-
-  <router-view />
+  <Navbar></Navbar>
 </template>
 
 <style lang="scss">
 @import "./assets/all.scss";
 </style>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  components: { Navbar },
+
+  methods: {},
+  // created() {
+  //   this.getProducts();
+  // },
+};
+</script>
