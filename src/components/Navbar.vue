0b740fc0985f2f1814d643c3e307f9f0041b1a59
c711cat <c711cat@gmail.com>
2023-11-16 08:52:01 +0800

    導覽列縮小時取代的icon修改成UI數計師指定樣式

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,7 +11,19 @@
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="36"
+          height="36"
+          fill="currentColor"
+          class="bi bi-list-ul"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"
+          />
+        </svg>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
@@ -75,5 +87,14 @@ export default {
 .btn:hover {
   background: #636af9;
   border: 1px solid #636af9;
+}
+
+.navbar-toggler {
+  border: none;
+  color: #474fde;
+}
+
+.navbar-toggler:hover {
+  color: #636af9;
 }
 </style>
