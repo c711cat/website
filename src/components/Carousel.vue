6880803f0bd0cb09a105f874ec728866d31bcaca
c711cat <c711cat@gmail.com>
2023-11-22 11:57:38 +0800

    增加手機版及平板的banner圖片,及 lg以上顯示網頁版圖片,lg以下顯示手機及平板版圖片

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -8,7 +8,9 @@
     data-bs-interval="3000"
     data-bs-pause="hover"
   >
-    <div class="carousel-inner">
+    <div
+      class="carousel-inner d-none d-sm-none d-md-none d-lg-block d-xl-block d-xxl-block"
+    >
       <div class="carousel-item active">
         <img src="@/img/banner1.png" class="d-block w-100" alt="bannerImg1" />
       </div>
@@ -17,6 +19,32 @@
       </div>
       <div class="carousel-item">
         <img src="@/img/banner3.png" class="d-block w-100" alt="bannerImg3" />
+      </div>
+    </div>
+
+    <div
+      class="carousel-inner d-block d-sm-block d-md-block d-lg-none d-xl-none d-xxl-none"
+    >
+      <div class="carousel-item active">
+        <img
+          src="@/img/phone_banner1.png"
+          class="d-block w-100"
+          alt="bannerImg1"
+        />
+      </div>
+      <div class="carousel-item">
+        <img
+          src="@/img/phone_banner2.png"
+          class="d-block w-100"
+          alt="bannerImg2"
+        />
+      </div>
+      <div class="carousel-item">
+        <img
+          src="@/img/phone_banner3.png"
+          class="d-block w-100"
+          alt="bannerImg3"
+        />
       </div>
     </div>
   </div>
