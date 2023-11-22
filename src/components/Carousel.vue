5c0a75877dd9176ddeecc41061232fa23865e38f
c711cat <c711cat@gmail.com>
2023-11-22 14:45:06 +0800

    因為兩個div包覆圖片時，輪播到最後一張圖之後下一張會出現空白的畫面，改成在img下顯示區別則可解決此問題

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -8,41 +8,40 @@
     data-bs-interval="3000"
     data-bs-pause="hover"
   >
-    <div
-      class="carousel-inner d-none d-sm-none d-md-none d-lg-block d-xl-block d-xxl-block"
-    >
-      <div class="carousel-item active">
-        <img src="@/img/banner1.png" class="d-block w-100" alt="bannerImg1" />
-      </div>
-      <div class="carousel-item">
-        <img src="@/img/banner2.png" class="d-block w-100" alt="bannerImg2" />
-      </div>
-      <div class="carousel-item">
-        <img src="@/img/banner3.png" class="d-block w-100" alt="bannerImg3" />
-      </div>
-    </div>
-
-    <div
-      class="carousel-inner d-block d-sm-block d-md-block d-lg-none d-xl-none d-xxl-none"
-    >
+    <div class="carousel-inner">
       <div class="carousel-item active">
         <img
+          src="@/img/banner1.png"
+          class="d-block w-100 d-none d-sm-none d-md-none d-lg-block d-xl-block d-xxl-block"
+          alt="bannerImg1"
+        />
+        <img
           src="@/img/phone_banner1.png"
-          class="d-block w-100"
+          class="d-block w-100 d-block d-sm-block d-md-block d-lg-none d-xl-none d-xxl-none"
           alt="bannerImg1"
         />
       </div>
       <div class="carousel-item">
         <img
+          src="@/img/banner2.png"
+          class="d-block w-100 d-none d-sm-none d-md-none d-lg-block d-xl-block d-xxl-block"
+          alt="bannerImg2"
+        />
+        <img
           src="@/img/phone_banner2.png"
-          class="d-block w-100"
+          class="d-block w-100 d-block d-sm-block d-md-block d-lg-none d-xl-none d-xxl-none"
           alt="bannerImg2"
         />
       </div>
       <div class="carousel-item">
         <img
+          src="@/img/banner3.png"
+          class="d-block w-100 d-none d-sm-none d-md-none d-lg-block d-xl-block d-xxl-block"
+          alt="bannerImg3"
+        />
+        <img
           src="@/img/phone_banner3.png"
-          class="d-block w-100"
+          class="d-block w-100 d-block d-sm-block d-md-block d-lg-none d-xl-none d-xxl-none"
           alt="bannerImg3"
         />
       </div>
@@ -56,7 +55,7 @@ export default {
   data() {
     return {
       carousel: {},
-      mounted() {
+      created() {
         this.carousel = new Carousel(this.$refs.carouselBody);
       },
     };
